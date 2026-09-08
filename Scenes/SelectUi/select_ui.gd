7b482a8ea30becf3af3dd5b8efc87e6943d66d43
TNdevour <arksolar03@gmail.com>
2026-09-08 08:10:16 +0300

    Refactor scene change to use move_to_master function

--- Scenes/SelectUi/select_ui.gd
+++ Scenes/SelectUi/select_ui.gd
@@ -9,8 +9,11 @@ func _ready() -> void:
 
 func _on_easy_mode_button_pressed() -> void:
 	SignalHub.emit_on_difficulty_set(EASY_MODE)
-	get_tree().change_scene_to_packed(MASTER)
+	move_to_master()
 
 func _on_hard_mode_button_pressed() -> void:
 	SignalHub.emit_on_difficulty_set(HARD_MODE)
+	move_to_master()
+
+func move_to_master()-> void:
 	get_tree().change_scene_to_packed(MASTER)
